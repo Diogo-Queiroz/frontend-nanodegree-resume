522f988a74768b787a46f53d1f2829a2a0bd4a91
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-04-11 23:16:20 -0300

    Initiate the transformation into a resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//$('#main').append('Diogo Fernandes de Queiroz');
+// var awesomeThoughts = "I am Diogo Queiroz and I am AWESOME!";
+// console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN');
+// $('#main').append(funThoughts);
+var name = 'Diogo Fernanes de Queiroz';
+var role = 'Game Developer / Programmer';
+var formattedName = HTMLheaderName.replace('%data%', name);
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
