0355cd367c944d69c980e459efb3577ac08cf72a
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-08-02 00:16:17 -0300

    some improvements on resume builder. maybe merging now and create another branch for each section it will keep things organized!

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,47 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-//$('#main').append('Diogo Fernandes de Queiroz');
+//$('#main').prepend('Diogo Fernandes de Queiroz');
 // var awesomeThoughts = "I am Diogo Queiroz and I am AWESOME!";
 // console.log(awesomeThoughts);
 // var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN');
-// $('#main').append(funThoughts);
+// $('#main').prepend(funThoughts);
 var name = 'Diogo Fernanes de Queiroz';
 var role = 'Web Developer / Game Programmer';
+var contacts = {
+    'email': 'diogoqueirooz1212@gmail.com',
+    'mobile': '+5561992528778',
+    'twitter': '@diogo-games',
+    'github': 'Diogo-Queiroz',
+    'blog': 'https://www.diogoqueiroz.com',
+    'location': 'Brasília/DF'
+};
+var welcomeMessage = 'Be welcome to my amazing resume build it with Javascript';
 var skills = ['programming', 'unreal engine', 'C++', 'JavaScript'];
-var bio = {
-    "name": name,
-    "role": role,
-    "contact": 'diogoqueirooz1212@gmail.com',
-    "picture_URL": "../images/fry.jpg",
-    "age": 30,
-    "skills": skills
+var biopic = './images/fry.jpg';
+var display = function display() {
+    return 0;
 };
 
-var formattedName = HTMLheaderName.replace('%data%', bio.name);
-var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-var formattedContact = HTMLcontactGeneric.replace('%data%', bio.contact);
-var formattedBioPic = HTMLbioPic.replace('%data%', bio.picture_URL);
+var formattedName = HTMLheaderName.replace('%data%', name);
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+var formattedBioPic = HTMLbioPic.replace('%data%', biopic);
+var formattedMobile = HTMLmobile.replace('%data%', contacts['mobile']);
+var formattedEmail = HTMLemail.replace('%data%', contacts['email']);
+var formattedTwitter = HTMLtwitter.replace('%data%', contacts['twitter']);
+var formattedGithub = HTMLgithub.replace('%data%', contacts['github']);
+var formattedBlog = HTMLblog.replace('%data%', contacts['blog']);
+var formattedLocation = HTMLlocation.replace('%data%', contacts['location']);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace('%data%', welcomeMessage);
+var formattedSkills = HTMLskills.replace('%data%', skills);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
-$('#header').append(formattedContact);
-$('#header').prepend(formattedBioPic);
+$('#header').append(formattedBioPic);
+$('#header').append(formattedWelcomeMessage);
+$('#header').append(formattedSkills);
+$('#topContacts').append(formattedMobile);
+$('#topContacts').prepend(formattedEmail);
+$('#topContacts').prepend(formattedTwitter);
+$('#topContacts').prepend(formattedGithub);
+$('#topContacts').prepend(formattedBlog);
+$('#topContacts').prepend(formattedLocation);
