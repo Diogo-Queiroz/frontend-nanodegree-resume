05948be681aac9e3079fb1fb7dd7dd2a38be3bea
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2018-08-02 02:42:48 +0000

    some updates using object notation in biography

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,7 +8,21 @@ This is empty on purpose! Your code to build the resume will go here.
 // $('#main').append(funThoughts);
 var name = 'Diogo Fernanes de Queiroz';
 var role = 'Web Developer / Game Programmer';
-var formattedName = HTMLheaderName.replace('%data%', name);
-var formattedRole = HTMLheaderRole.replace('%data%', role);
+var skills = ['programming', 'unreal engine', 'C++', 'JavaScript'];
+var bio = {
+    "name": name,
+    "role": role,
+    "contact": 'diogoqueirooz1212@gmail.com',
+    "picture_URL": "../images/fry.jpg",
+    "age": 30,
+    "skills": skills
+};
+
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+var formattedContact = HTMLcontactGeneric.replace('%data%', bio.contact);
+var formattedBioPic = HTMLbioPic.replace('%data%', bio.picture_URL);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
+$('#header').append(formattedContact);
+$('#header').prepend(formattedBioPic);
